4b898dfb0c6ac1c2df1a3ecd459b3dd1f6f32f61
Muhammad Tifak <KingGenius5@users.noreply.github.com>
2019-09-13 10:43:11 -0700

    Fixed json function to get response

--- app.py
+++ app.py
@@ -4,53 +4,41 @@ import json
 
 app = Flask(__name__)
 
-# setting the apikey and limit
-#this is based off of the tenor api (https://tenor.com/gifapi/documentation#quickstart-search)
-apikey = "F4742JEU9YNK"  #test value - apikey came from tenor developer dashboard
-
+'''
+This is based off of reworking the tenor api resource, a great helper
+(https://tenor.com/gifapi/documentation#quickstart-search)
+'''
 
 @app.route('/')
 def index():
     """Return homepage."""
     # TODO: Extract the query term from url using request.args.get()
     user_input = request.args.get('user_input')
-    
-    query_term = "kevin"
 
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
     params = {
-        "q": query_term,
-        "Key": "F4742JEU9YNK",
+        "q": user_input,
+        "Key": "F4742JEU9YNK",#api key came from tenor developer dashboard
         "limit": 10
     }
 
     # TODO: Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
-
-    # get the top 10 GIFs for the search term
-    response = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s", params=params)
-
-    if response.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
-        top_10gifs = json.loads(response.content)
-        print(top_10gifs)
-    else:
-        top_10gifs = None
+    response = requests.get("https://api.tenor.com/v1/search", params)
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
-
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
+    top_10gifs = json.loads(response.content)['results']
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
-
-    return render_template("index.html", top_10gifs=top_10gifs, user_input=user_input)
+    return render_template("index.html", top_10gifs=top_10gifs)
 
 if __name__ == '__main__':
     app.run(debug=True)
