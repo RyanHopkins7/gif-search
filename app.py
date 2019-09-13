2a5506396e8ac5824304f491e7138cdfddd8c7f6
Muhammad Tifak <moht20009@gmail.com>
2019-09-12 22:43:55 -0700

    Can make an API call to Tenor now

--- app.py
+++ app.py
@@ -23,6 +23,24 @@ def index():
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
 
+    '''
+    Based off of the tenor api code provided, will change accordingly
+    '''
+
+    # our test search
+    search_term = "excited"
+
+    # get the top 8 GIFs for the search term
+    r = requests.get(
+        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        top_10gifs = json.loads(r.content)
+        print top_8gifs
+    else:
+        top_10gifs = None
+
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
 
