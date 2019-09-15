b96233552537b40c23922b0862c199f81b0107f7
RyanHopkins7 <ryanalexanderhopkins@gmail.com>
2019-09-15 10:09:48 -0700

    Add typeAhead script

---
+++ static/typeAhead.js
@@ -0,0 +1,8 @@
+searchBar = document.getElementById("search-bar");
+
+searchBar.onkeypress = function() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "/user_input=" + searchBar.value, true);
+    xhttp.send();
+}
+
