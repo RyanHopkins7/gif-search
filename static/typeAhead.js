cd5c6e8d8cdbba49e628f7a164ed6c94f80bfea1
RyanHopkins7 <ryanalexanderhopkins@gmail.com>
2019-09-15 11:10:11 -0700

    Comment out typeAhead.js until functional

--- static/typeAhead.js
+++ static/typeAhead.js
@@ -1,8 +1,9 @@
 searchBar = document.getElementById("search-bar");
 
+/*
 searchBar.onkeypress = function() {
     let xhttp = new XMLHttpRequest();
     xhttp.open("GET", "/user_input=" + searchBar.value, true);
     xhttp.send();
 }
-
+*/
