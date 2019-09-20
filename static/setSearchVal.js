88944d214e8c4fa55bc713682b10b1d2e5c3b018
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-09-20 14:42:25 -0700

    Only send post requests every 300ms rather than every keystroke to improve speed

--- static/setSearchVal.js
+++ static/setSearchVal.js
@@ -1,6 +1,6 @@
 // Sets text in search bar to search query from get request
 searchBar = document.getElementById("search-bar");
 if (window.location.search.includes("user_input")) {
-    searchQuery = window.location.search.substring(12)
-    searchBar.value = searchQuery.replace(/\+/g, " ")
+    searchQuery = window.location.search.substring(12);
+    searchBar.value = searchQuery.replace(/\+/g, " ");
 }
--- static/typeAhead.js
+++ static/typeAhead.js
@@ -1,19 +1,31 @@
 searchBar = document.getElementById("search-bar");
-gifWrapper = document.getElementById("gif-wrapper")
+gifWrapper = document.getElementById("gif-wrapper");
 
 // As user types, send POST requests to /typeahead route with search query as body
-searchBar.onkeyup = function() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/typeahead", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-    // When client receives gifs from server, display them
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4) {
-            gifWrapper.innerHTML = xhttp.responseText
-        }
-    }
+let sendRequest = true;
 
-    xhttp.send("user_input="+searchBar.value);
+searchBar.onkeyup = function () {
+    // Only send requests every 300 ms rather than every keystroke
+    if (sendRequest) {
+        sendRequest = false;
+        setTimeout(function () {
+            sendRequest = true;
+            console.log("nice");
+
+            let xhttp = new XMLHttpRequest();
+            xhttp.open("POST", "/typeahead", true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+            // When client receives gifs from server, display them
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4) {
+                    gifWrapper.innerHTML = xhttp.responseText;
+                }
+            }
+    
+            xhttp.send("user_input=" + searchBar.value);
+        }, 300)
+    }    
 }
 
